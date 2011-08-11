3792abd23920ccd5258f70b93b99139754c9ab00
Matthias Andreas Benkard <code@mail.matthias.benkard.de>
2011-08-11 13:03:47 +0200

    Fix primary file setting.

--- info.rkt
+++ info.rkt
@@ -7,15 +7,15 @@
          "JSON Template")
       ", a minimalistic, yet powerful, template language."))
 (define categories '(misc))
-(define version "1.0.2")
+(define version "1.0.4")
 (define can-be-loadded-with 'all)
-(define primary-file "json-template.rkt")
+(define primary-file (list "main.rkt"))
 (define homepage "http://matthias.benkard.de/software/json-template-for-racket")
 (define scribblings '(("manual.scrbl" ())))
 
-#;
 (define release-notes
-  ...)
+  '(p ()
+      "Fix primary file setting."))
 
 #;
 (define required-core-version
